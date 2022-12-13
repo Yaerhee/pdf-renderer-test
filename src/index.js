7e9194a183e6bc42bd7591b1d550457e0a79a3a8
yaerhee <yaerhee@naver.com>
2022-12-13 15:32:51 +0900

    changed content & example PageWrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {PDFViewer} from "@react-pdf/renderer";
+
+import PageWrap from './pageWrap';
+
+const Viewer = () => {
+    return (
+        <div className="wrapper">
+            <h2>Korean PDF Rendering Test</h2>
+            <PDFViewer style={{ flex: 1 }}>
+                <PageWrap />
+            </PDFViewer>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <Viewer />
+    </React.StrictMode>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
